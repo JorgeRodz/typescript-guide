326014012a60aaee0a5deb12544417433d117a92
Jorge Rodriguez <jarf4321@gmail.com>
2024-06-30 15:16:42 -0600

    [Add] export and import examples

---
+++ ts-node/src/index.ts
@@ -0,0 +1,9 @@
+import { Hero } from "./classes/Hero"
+// import { Hero as SuperHero } from "./classes/Hero";
+// import { Hero, Hero2, Hero3, Hero4, PI } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero"
+// import powers, { Power } from "./data/powers"
+
+const ironman = new Hero('Ironman', 1, 55)
+console.log(ironman)
+console.log(ironman.power)
